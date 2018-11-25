7172bb359fede32b3bd43481958f50d484ed9dca
TirthShah081296 <43168870+TirthShah081296@users.noreply.github.com>
2018-11-25 03:02:23 -0700

    Update main.py

--- project/phaseIII/main.py
+++ project/phaseIII/main.py
@@ -9,17 +9,19 @@ import numpy as np
 import numpy.linalg as la
 import scipy.cluster.vq as vq
 from scipy.sparse import csc_matrix
+from task5 import LSH
+from task6 import KNN
+
 
 class Interface():
 
     def __init__(self):
         self.__database__ = None
-        self.__graph__ = None 
+        self.__graph__ = None
         self.__valid_types__ = ['photo', 'user', 'poi']
         self.__vis_models__ = ['CM', 'CM3x3', 'CN', 'CN3x3',
-                'CSD', 'GLRLM', 'GLRLM3x3', 'HOG', 'LBP', 'LBP3x3']
+                               'CSD', 'GLRLM', 'GLRLM3x3', 'HOG', 'LBP', 'LBP3x3']
         self.__io__()
-    
 
     def __io__(self):
         print("Welcome to the CSE 515 data software. Please enter a command.\
@@ -27,21 +29,21 @@ class Interface():
 
         def __filepath__(parser, arg):
             if not isdir(arg):
-                parser.error("The directory %s doesn't exist." %arg)
+                parser.error("The directory %s doesn't exist." % arg)
             else:
                 return True
 
         parser = argparse.ArgumentParser()
-        parser.add_argument('-task', type=int, choices=range(1,7), required=True, metavar='#')
+        parser.add_argument('-task', type=int, choices=range(1, 7), required=True, metavar='#')
         parser.add_argument('--k', type=int, metavar='#')
         parser.add_argument('--alg', type=str, metavar="algorithm_to_use")
-        parser.add_argument('--imgs', type=int, nargs='+', metavar='img1 img2 ...')
+        parser.add_argument('--imgs', type=str, nargs='+', metavar='imageId')
         parser.add_argument('--load', type=str, metavar='filepath')
         parser.add_argument('--graph', type=str, metavar='filename')
         parser.add_argument('--layers', type=int, metavar='L')
         parser.add_argument('--hashes', type=int, metavar='k')
         # parser.add_argument('--cluster', type=int, metavar='c')
-        parser.add_argument('--vectors', type=str) #Assuming this is a file locaiton 
+        parser.add_argument('--vectors', type=str)  # Assuming this is a file locaiton
         while True:
             user_input = input("\nEnter a Command:$ ")
             user_input = user_input.split(' ')
@@ -64,7 +66,6 @@ class Interface():
                     print('Something went wrong during database load.')
                     print(e)
 
-
             # load the graph from the file.
             if args.graph:
                 try:
@@ -73,20 +74,18 @@ class Interface():
                     print('Something went wrong loading the graph.')
                     print(e)
 
-            # Get method with this name - makes it easy to create new interface methods 
+            # Get method with this name - makes it easy to create new interface methods
             #   without having to edit this method.
             try:
                 method = getattr(self, 'task' + str(args.task))
             except AttributeError:
                 print('The command specified was not a valid command.\n')
-            
+
             try:
                 method(args)
             except Exception as e:
                 print('Something went wrong while executing ' + str(method.__name__))
                 print(str(e))
-
-
 
     def help(self, args):
         """
@@ -99,7 +98,6 @@ class Interface():
             if not item.startswith('__'):
                 method = getattr(self, item)
                 print(method.__doc__)
-        
 
     def load(self, args):
         """
@@ -108,17 +106,15 @@ class Interface():
         Arguments:
         \t<filepath> - A valid file path in the system.
         """
-        
+
         folder = abspath(args.load)
 
         if not isdir(folder):
             print("[ERROR] The provided path was not a folder, and therefore not a valid data directory.")
             return
-        
+
         self.__database__ = Loader.make_database(folder)
         print("Database loaded successfully.")
-    
-
 
     def graph(self, args):
         """
@@ -132,11 +128,9 @@ class Interface():
         if not isfile(f):
             print("[ERROR] The provided path was not a valid file.")
             return
-        
+
         self.__graph__ = Graph.load(f)
         print('Graph loaded successfully.')
-
-
 
     @timed
     def task1(self, args):
@@ -147,8 +141,6 @@ class Interface():
             self.__graph__ = Loader.make_graphs(self.__database__, k)
         # visualize graph.
         self.__graph__.display()
-    
-
 
     def task2(self, args):
         if args.k == None:
@@ -156,7 +148,7 @@ class Interface():
         c = int(args.k)
         # alg = args.alg
 
-        #YOUR CODE HERE.
+        # YOUR CODE HERE.
         clusters = {}
         clusters1 = {}
         images = self.__graph__.get_images()
@@ -166,7 +158,7 @@ class Interface():
         lengOfB = 0
         lengOfClusters = {}
         A = self.__graph__.get_adjacency()
-        D = np.diag(np.ravel(np.sum(A,axis=1)))
+        D = np.diag(np.ravel(np.sum(A, axis=1)))
         L = D - A
         l, U = la.eigh(L)
         f = U[:, 1]
@@ -174,7 +166,7 @@ class Interface():
         # Clustering function
         for image in images:
 
-            if(labels[images.index(image)] == -1):
+            if (labels[images.index(image)] == -1):
                 cluster = 'A'
                 clusters[image] = cluster
                 lengOfA += 1
@@ -195,8 +187,8 @@ class Interface():
         print("Clusters in A:", lengOfA)
         print("Clusters in B:", lengOfB)
 
-        xx = c+1
-        means, labels1 = vq.kmeans2(U[:, 1:xx], c, iter= 500)
+        xx = c + 1
+        means, labels1 = vq.kmeans2(U[:, 1:xx], c, iter=500)
         for j in range(c):
             indices = [i for i, x in enumerate(labels1) if x == j]
             lengOfClusters[j] = len(indices)
@@ -209,9 +201,6 @@ class Interface():
             self.__graph__.add_to_cluster(image, clusters1[image])
         self.__graph__.display(clusters=list_of_clusters1, filename='task2_kspectral.png')
         print(lengOfClusters)
-
-
-
 
     def task3(self, args):
         if args.k == None:
@@ -263,7 +252,6 @@ class Interface():
         print(listOfImages)
         pass
 
-
     @timed
     def task4(self, args):
         if args.k == None or args.imgs == None:
@@ -295,7 +283,7 @@ class Interface():
             Ei[ii] = 1 / len(imgs)
         # Compute pagerank r until we converge
         ro, r = np.zeros(n), np.ones(n)
-        #while np.sum(np.abs(r - ro)) > maxerr:
+        # while np.sum(np.abs(r - ro)) > maxerr:
         for _ in range(100):
 
             if np.sum(np.abs(r - ro)) <= maxerr:
@@ -310,7 +298,6 @@ class Interface():
                 # Di = sink / float(n)
                 # account for teleportation to state i
 
-
                 r[i] = ro.dot(Ai * s + Ei * (1 - s))
 
         weights = r / float(sum(r))
@@ -324,37 +311,103 @@ class Interface():
         print(listOfImages)
         pass
 
-
-    
     def task5(self, args):
         """
         Use as:
         -task 5 --layers # --hashes # --k # --imgs #
         """
         if args.layers == None or args.hashes == None or \
-            args.k == None or args.imgs == None:
+                args.k == None or args.imgs == None:
             raise ValueError('Layers, Hashes, Vectors, K, and IMG must all be defined for task 5.')
-        
+
         layers = int(args.layers)
         hashes = int(args.hashes)
         t = int(args.k)
-        imgs = list(args.imgs)
+        imageId = args.imgs
         if args.vectors:
             vectors = str(args.vectors)
-    
-        # YOUR CODE HERE
-    
 
+        # YOUR CODE HERE
+        lsh = LSH()
+        lsh.main(layers, hashes, imageId, vectors=(), t=t, database=self.__database__)
 
     def task6(self, args):
         if args.alg == None:
             raise ValueError('Alg must be defined for task 6.')
-        
+
         alg = str(args.alg)
 
         # YOUR CODE HERE
+        if alg == "knn":
+            k = 3
+            imageIDs = ['3298433827', '299114458', '948633075', '4815295122', '5898734700', '4027646409', '1806444675',
+                        '4501766904', '6669397377', '3630226176', '3630226176', '3779303606', '4017014699']
+            labels = ['fort', 'sculpture', 'sculpture', 'sculpture', 'sculpture', 'fort', 'fort', 'fort', 'sculpture',
+                      'sculpture', 'sculpture', 'sculpture', 'sculpture']
+            '''
+            j = 0
+            for i in args:
+                if j % 2 == 0:
+                    imageIDs.append([i])
+                else:
+                    labels.append([i])
+                j = j + 1
+            '''
+            result = KNN.knn_algorithm(imageIDs, labels, k, self.__database__)
+            print("result: " + str(result))
 
+        elif alg == "ppr":
+            G = self.__graph__.get_adjacency()
+            images = self.__graph__.get_images()
+            indexes = list()
 
+            imageIDs = ['3298433827', '299114458', '948633075', '4815295122', '5898734700', '4027646409', '1806444675',
+                        '4501766904', '6669397377', '3630226176', '3630226176', '3779303606', '4017014699']
+            labels = ['fort', 'sculpture', 'sculpture', 'sculpture', 'sculpture', 'fort', 'fort', 'fort', 'sculpture',
+                      'sculpture', 'sculpture', 'sculpture', 'sculpture']
+
+            for x in imageIDs:
+                indexes.append(images.index(x))
+            n = G.shape[0]
+            s = 0.86
+            maxerr = 0.1
+
+            # transform G into markov matrix A
+            A = csc_matrix(G, dtype=np.float)
+            rsums = np.array(A.sum(1))[:, 0]
+            ri, ci = A.nonzero()
+            A.data /= rsums[ri]
+
+            # bool array of sink states
+            sink = rsums == 0
+
+            Ei = np.zeros(n)
+            for ii in indexes:
+                Ei[ii] = 1 / len(imageIDs)
+            # Compute pagerank r until we converge
+            ro, r = np.zeros(n), np.ones(n)
+            # while np.sum(np.abs(r - ro)) > maxerr:
+            for _ in range(100):
+
+                if np.sum(np.abs(r - ro)) <= maxerr:
+                    break
+
+                ro = r.copy()
+                # calculate each pagerank at a time
+                for i in range(0, n):
+                    # in-links of state i
+                    Ai = np.array(A[:, i].todense())[:, 0]
+                    # account for sink states
+                    # Di = sink / float(n)
+                    # account for teleportation to state i
+
+                    r[i] = ro.dot(Ai * s + Ei * (1 - s))
+
+            weights = r / float(sum(r))
+            orderedWeights = np.argsort(weights)
+            ReorderedWeights = np.flipud(orderedWeights)
+
+            # gotta do something now
 
     def quit(self, *args):
         """
@@ -362,6 +415,7 @@ class Interface():
         Description:\tExits the program and performs necessary cleanup.
         """
         exit(1)
+
 
 if __name__ == '__main__':
     Interface()
